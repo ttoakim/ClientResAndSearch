dcf30128956a28273257469cbe256e50d200a00f
준혁 김 <ffgffg343@gmail.com>
2018-06-04 16:59:04 +0900

    [update] 버튼추가

--- GitHubTest/GitHubTest/Form1.cs
+++ GitHubTest/GitHubTest/Form1.cs
@@ -12,9 +12,15 @@ namespace GitHubTest
 {
     public partial class Form1 : Form
     {
+        public Button btn;
         public Form1()
         {
             InitializeComponent();
+            btn = new Button();
+            btn.Location = new Point(50,50);
+            btn.Text = "수정버튼";
+            btn.Size = new Size(30,20);
+            Controls.Add(btn);
         }
     }
 }
